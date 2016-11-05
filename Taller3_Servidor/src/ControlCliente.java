50d06b798be13211d1afbc8cd6e2638ec215be7c
sky_k <sky_karlos@hotmail.com>
2016-11-04 22:02:30 -0500

    fixes

--- Taller3_Servidor/src/ControlCliente.java
+++ Taller3_Servidor/src/ControlCliente.java
@@ -1,6 +1,9 @@
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
@@ -59,11 +62,25 @@ public class ControlCliente extends Observable implements Runnable {
 			Post p= (Post) o;
 			System.out.println(p.nombreUsuario);
 			System.out.println(p.contenidoPost);
+			guardarArchivo(p.nombreUsuario+"mirameEstoPapu.png", p.imagen);
 		}else if(o instanceof String){
 		
 		String mensaje = (String) o;
 		System.out.println("[ MENSAJE A RECIBIDO: " + mensaje + " ]");
 		jefe.update(this, mensaje);
+		}
+	}
+	
+	private void guardarArchivo(String nombre, byte[] buf) throws IOException {
+		try {
+			File archivo = new File("data/" + nombre);
+			archivo.createNewFile();
+			FileOutputStream salida = new FileOutputStream(archivo);
+			salida.write(buf);
+			salida.flush();
+			salida.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 	}
 
